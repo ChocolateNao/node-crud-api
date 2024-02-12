3c2b1589f73a2828cdfffc07616e6ffb45b21548
Andrey Loginov <andrey12q112@gmail.com>
2024-02-13 02:53:22 +0300

    test: add tests

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,6 @@ const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
-  setupFilesAfterEnv: ['<rootDir>/tests/setupTests.ts'],
   clearMocks: true,
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts', '!tests/*.test.ts'],
